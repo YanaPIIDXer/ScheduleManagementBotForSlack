7dc08c55b033d3a36b88b051eade5d66142c989e
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-05-02 23:24:11 +0900

    ChatGPTの使用準備

--- src/app.py
+++ src/app.py
@@ -10,11 +10,19 @@ slack_bot_token = os.environ["SLACK_BOT_TOKEN"]
 
 app = App(token=slack_bot_token, process_before_response=True)
 
+# ChatGPTに投げる
+def think(channel, thread_ts):
+    pass
+
+# メンション受信イベント
 @app.event("app_mention")
 def handle_mention(event, say, logger):
-    logger.info(event)
     message = re.sub(r'^<.*>', '', event['text'])   # メンション除去
+    channel = event["channel"]
     thread_ts = event.get("thread_ts", None) or event["ts"]     # スレッド
+
+    think(channel, thread_ts)
+
     # とりあえずオウム返し
     say(f"<@{event['user']}>{message}", thread_ts=thread_ts)
 

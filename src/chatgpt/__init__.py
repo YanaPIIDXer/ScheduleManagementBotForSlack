13c0e62964c0e0c41fad533baa7ce36ad5bb37c2
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-05-03 06:00:10 +0900

    ChatBotクラス実装準備

---
+++ src/chatgpt/__init__.py
@@ -0,0 +1,2 @@
+from .chatbot import ChatBot
+from .prompts import *
---
+++ src/chatgpt/chatbot.py
@@ -0,0 +1,16 @@
+class ChatBot:
+    """チャットボットクラス
+
+    ChatGPTを使ったチャットボットクラス
+
+    Attributes:
+    None yet.
+
+    Methods:
+    __init__: コンストラクタ
+    """
+    def __init__(self):
+        """コンストラクタ
+        """
+        pass
+    

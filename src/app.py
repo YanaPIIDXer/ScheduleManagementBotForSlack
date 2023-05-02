331bd3fa7a54087b8abf9d0201c191f6a0b6f044
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-05-03 07:25:59 +0900

    カレンダーに登録した旨の通知 ※実際に登録する処理はまだ

--- src/app.py
+++ src/app.py
@@ -11,27 +11,51 @@ slack_bot_token = os.environ["SLACK_BOT_TOKEN"]
 app = App(token=slack_bot_token, process_before_response=True)
 
 # ChatGPTに投げる
-def think(channel, message, thread_ts, say):
+def think(channel, message, thread_ts, sender, say):
     bot = ChatBot(os.environ["OPENAI_API_KEY"])
     bot.set_simple_template(promptTemplate, ["input"])
-    result = bot.run({ "input": message })
-    say(result, thread_ts=thread_ts)
+    summarizedJsonText = bot.run({ "input": message })
+    summarizedData = json.loads(summarizedJsonText)
+    if summarizedData["is_valid"] == False:
+        msg = f"<@{sender}>スケジュールを抽出できませんでした"
+        return say(msg, thread_ts)
+    
+    start_dt = summarizedData["start_date_time"]
+    end_dt = summarizedData["end_date_time"]
+    title = summarizedData["title"]
+    summary = summarizedData["summary"]
+    # TODO: 実際にGoogleカレンダーに登録する処理の実装
+    msg = f"""
+    <@{sender}>
+    以下の内容でカレンダーに登録しました。
+    
+    タイトル:
+    {title}
+    説明:
+    {summary}
+    開始日時:
+    {start_dt}
+    終了日時:
+    {end_dt}
+    """
+    say(msg, thread_ts=thread_ts)
 
 # メンション受信イベント
 @app.event("app_mention")
-def handle_mention(event, say, logger):
+def handle_mention(event, say):
     message = re.sub(r'^<.*>', '', event['text'])   # メンション除去
     channel = event["channel"]
     thread_ts = event.get("thread_ts", None) or event["ts"]     # スレッド
 
-    think(channel, message, thread_ts, say)
+    think(channel, message, thread_ts, event["user"], say)
 
 SlackRequestHandler.clear_all_log_handlers()
 logging.basicConfig(format="%(asctime)s %(message)s", level=logging.DEBUG)
 
 def handler(event, context):
     # HACK: リトライは弾く
-    if "X-Slack-Retry-Num" in event["headers"]:
+    if "X-Slack-Retry-Num" in event["headers"] and int(event["headers"]["X-Slack-Retry-Num"]) > 1:
+        print("Retry:" + event["headers"]["X-Slack-Retry-Num"])
         return {
             "statusCode": 200,
             "body": json.dumps("OK"),
